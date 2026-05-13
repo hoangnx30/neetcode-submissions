21f3787cf92211d1ba895d9038ada357ef6cb7a1
Hoang Nguyen <43129138+hoangnx30@users.noreply.github.com>
2026-05-13 18:39:39 +0700

    Add: remove-element - submission-0

---
+++ Data Structures & Algorithms/remove-element/submission-0.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} val
+     * @return {number}
+     */
+    removeElement(nums, val) {
+        const temp = [];
+
+        for (const num of nums) {
+            if (num !== val) {
+                temp.push(num);
+            }
+        }
+
+        for (let i = 0; i < temp.length; i++) {
+            nums[i] = temp[i];
+        }
+
+        return temp.length;
+    }
+}
