d16ac4914d9f9e474b67733f65095755286e83dc
Hoang Nguyen <43129138+hoangnx30@users.noreply.github.com>
2026-05-13 21:40:41 +0700

    Add: isomorphic-strings - submission-6

---
+++ Data Structures & Algorithms/isomorphic-strings/submission-6.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isIsomorphic(s, t) {
+        const mapS = new Map();
+        const mapT = new Map();
+
+        for (let i = 0; i < s.length; i++) {
+            if (mapS.has(s[i]) && mapS.get(s[i]) !== t[i]) return false;
+
+            if (mapT.has(t[i]) && mapT.get(t[i]) !== s[i]) return false;
+
+            mapS.set(s[i], t[i]);
+            mapT.set(t[i], s[i]);
+        }
+
+        return true
+    }
+}
