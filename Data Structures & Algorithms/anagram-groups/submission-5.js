1103a6ea58e6423fb873f0f677bc83d92640e643
Hoang Nguyen <43129138+hoangnx30@users.noreply.github.com>
2026-05-17 22:04:39 +0700

    Add: anagram-groups - submission-5

---
+++ Data Structures & Algorithms/anagram-groups/submission-5.js
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+        const result = {};
+
+        for (const str of strs) {
+            const count = new Array(26).fill(0);
+
+            for (const c of str) {
+                count[c.charCodeAt(0) - "a".charCodeAt(0)] += 1;
+            }
+            console.log(count);
+
+            const key = count.join("-");
+
+            if (!result[key]) {
+                result[key] = [];
+            }
+
+            result[key].push(str);
+        }
+
+        return Object.values(result);
+    }
+}
