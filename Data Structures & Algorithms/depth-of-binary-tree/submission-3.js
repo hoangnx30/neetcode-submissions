8d4712bf77eb83f3ee7afb64ea492212043b94c1
Hoang Nguyen <43129138+hoangnx30@users.noreply.github.com>
2026-07-05 21:45:21 +0700

    Add: depth-of-binary-tree - submission-3

---
+++ Data Structures & Algorithms/depth-of-binary-tree/submission-3.js
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {number}
+     */
+    maxDepth(root) {
+        if (!root) {
+            return 0;
+        }
+
+        const queue = [root];
+
+        let result = 0;
+        while (queue.length > 0) {
+            const length = queue.length
+            for (let i = 0; i < length; i++) {
+                const node = queue.shift()
+                if (node.left) {
+                    queue.push(node.left);
+                }
+
+                if (node.right) {
+                    queue.push(node.right);
+                }
+            }
+            result++;
+        }
+
+        return result;
+    }
+}
