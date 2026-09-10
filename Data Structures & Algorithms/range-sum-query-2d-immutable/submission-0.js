fdedc6694cc359b96b75b3b0876d98e216d6e5c7
Hoang Nguyen <43129138+hoangnx30@users.noreply.github.com>
2026-09-10 20:35:47 +0800

    Add: range-sum-query-2d-immutable - submission-0

---
+++ Data Structures & Algorithms/range-sum-query-2d-immutable/submission-0.js
@@ -0,0 +1,33 @@
+class NumMatrix {
+    /**
+     * @param {number[][]} matrix
+     */
+    matrix = []
+    constructor(matrix) {
+        this.matrix = matrix
+    }
+
+    /**
+     * @param {number} row1
+     * @param {number} col1
+     * @param {number} row2
+     * @param {number} col2
+     * @return {number}
+     */
+    sumRegion(row1, col1, row2, col2) {
+        let result = 0
+        for (let i = row1; i <= row2; i++) {
+            for (let j = col1; j <= col2; j++) {
+                result += this.matrix[i][j]
+            }
+        }
+
+        return result
+    }
+}
+
+/**
+ * Your NumMatrix object will be instantiated and called as such:
+ * var obj = new NumMatrix(matrix)
+ * var param_1 = obj.sumRegion(row1,col1,row2,col2)
+ */
